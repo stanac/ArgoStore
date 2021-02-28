925a60e663feb4ca43b19f6ad828282e0206b73a
Milan Stanacev <stanacevm@gmail.com>
2021-02-28 18:41:03 +0100

    select remapping test

--- tests/ArgoStore.UnitTests/StatementsTests/SelectStatementTests.cs
+++ tests/ArgoStore.UnitTests/StatementsTests/SelectStatementTests.cs
@@ -1,13 +1,9 @@
 ﻿using ArgoStore.ExpressionToStatementTranslators;
 using ArgoStore.Helpers;
 using FluentAssertions;
-using Moq;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace ArgoStore.UnitTests.StatementsTests
