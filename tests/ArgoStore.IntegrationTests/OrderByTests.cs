1efc30f2827d490788fd6d4e9d6dc5803074a2d7
Milan Stanacev <milan@stanacev.com>
2021-04-23 14:38:28 +0200

    order by integration test

---
+++ tests/ArgoStore.IntegrationTests/OrderByTests.cs
@@ -0,0 +1,40 @@
+﻿using ArgoStore.IntegrationTests.Entities;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ArgoStore.IntegrationTests
+{
+    public class OrderByTests : IntegrationTestsBase
+    {
+        public OrderByTests()
+        {
+            using (IDocumentSession session = GetNewDocumentSession())
+            {
+                TestData td = new TestData(TestDbConnectionString);
+                td.InsertTestPersons();
+            }
+        }
+
+        [Fact]
+        public void Test()
+        {
+            using (IDocumentSession session = GetNewDocumentSession())
+            {
+                List<Person> persons = session.Query<Person>()
+                    .OrderByDescending(x => x.Name)
+                    .Where(x => x.BirthYear.HasValue)
+                    .ToList();
+
+                int count = persons.Count;
+                count.Should().Be(1);
+
+                persons.First().Name.Should().Be("1");
+            }
+        }
+    }
+}
