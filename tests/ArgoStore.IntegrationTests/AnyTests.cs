534c6be41f3e51083182f640d5062bb397963315
Milan Stanacev <stanacevm@gmail.com>
2021-06-20 11:16:33 +0200

    integration tests for any, count and longcount

---
+++ tests/ArgoStore.IntegrationTests/AnyTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using ArgoStore.IntegrationTests.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace ArgoStore.IntegrationTests
+{
+    public class AnyTests : IntegrationTestsBase
+    {
+        private const string TestNameImogenCampbell = "Imogen Campbell";
+        private const string TestNameNonExisting = "Someone who is not in test DB";
+
+        private readonly TestData _td;
+
+        public AnyTests()
+        {
+            _td = new TestData(TestDbConnectionString);
+
+            using IDocumentSession session = GetNewDocumentSession();
+            _td.InsertTestPersons();
+        }
+
+        [Fact]
+        public void Any_ReturnsExpectedResult()
+        {
+            using IDocumentSession session = GetNewDocumentSession();
+
+            bool exists = session.Query<Person>().Any();
+            exists.Should().BeTrue();
+        }
+        
+        [Fact]
+        public void AnyWithCondition_ReturnsExpectedResult()
+        {
+            using IDocumentSession session = GetNewDocumentSession();
+
+            bool exists = session.Query<Person>().Any(x => x.Name == TestNameImogenCampbell);
+            exists.Should().BeTrue();
+        }
+
+        [Fact]
+        public void AnyWithCondition_NonExistingPerson_ReturnsExpectedResult()
+        {
+            using IDocumentSession session = GetNewDocumentSession();
+
+            bool exists = session.Query<Person>().Any(x => x.Name == TestNameNonExisting);
+            exists.Should().BeFalse();
+        
+    }
+}
---
+++ tests/ArgoStore.IntegrationTests/CountTests.cs
@@ -0,0 +1,77 @@
+﻿using System.Linq;
+using ArgoStore.IntegrationTests.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace ArgoStore.IntegrationTests
+{
+    public class CountTests : IntegrationTestsBase
+    {
+        private const string TestNameImogenCampbell = "Imogen Campbell";
+        private const string TestNameNonExisting = "Someone who is not in test DB";
+
+        private readonly TestData _td;
+
+        public CountTests()
+        {
+            _td = new TestData(TestDbConnectionString);
+
+            using IDocumentSession session = GetNewDocumentSession();
+            _td.InsertTestPersons();
+        }
+        
+        [Fact]
+        public void Count_ReturnsExpectedCount()
+        {
+            using IDocumentSession session = GetNewDocumentSession();
+
+            int count = session.Query<Person>().Count();
+            count.Should().Be(_td.Persons.Count);
+        }
+
+        [Fact]
+        public void LongCount_ReturnsExpectedCount()
+        {
+            using IDocumentSession session = GetNewDocumentSession();
+
+            long count = session.Query<Person>().LongCount();
+            count.Should().Be(_td.Persons.Count);
+        }
+
+        [Fact]
+        public void CountWithCondition_ReturnsExpectedCount()
+        {
+            using IDocumentSession session = GetNewDocumentSession();
+
+            int count = session.Query<Person>().Count(x => x.Name == TestNameImogenCampbell);
+            count.Should().Be(1);
+        }
+
+        [Fact]
+        public void LongCountWithCondition_ReturnsExpectedCount()
+        {
+            using IDocumentSession session = GetNewDocumentSession();
+
+            long count = session.Query<Person>().LongCount(x => x.Name == TestNameImogenCampbell);
+            count.Should().Be(1);
+        }
+
+        [Fact]
+        public void CountWithCondition_NonExistingPerson_ReturnsExpectedCount()
+        {
+            using IDocumentSession session = GetNewDocumentSession();
+
+            int count = session.Query<Person>().Count(x => x.Name == TestNameNonExisting);
+            count.Should().Be(0);
+        }
+
+        [Fact]
+        public void LongCountWithCondition_NonExistingPerson_ReturnsExpectedCount()
+        {
+            using IDocumentSession session = GetNewDocumentSession();
+
+            long count = session.Query<Person>().LongCount(x => x.Name == TestNameNonExisting);
+            count.Should().Be(0);
+        }
+    }
+}
