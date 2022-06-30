e7d060b873ff41a8ee7cd806c700c8f523fe4bd1
stanac <stanacevm@gmail.com>
2022-06-30 21:29:21 +0200

    select transaltor tests

--- tests/ArgoStore.UnitTests/ExpressionToStatementTranslators/SelectExpressionTranslatorTests.cs
+++ tests/ArgoStore.UnitTests/ExpressionToStatementTranslators/SelectExpressionTranslatorTests.cs
@@ -1,6 +1,24 @@
-﻿namespace ArgoStore.UnitTests.ExpressionToStatementTranslators;
+﻿using ArgoStore.TestsCommon.Entities;
+
+namespace ArgoStore.UnitTests.ExpressionToStatementTranslators;
 
 public class SelectExpressionTranslatorTests
 {
+    [Fact]
+    public void Test()
+    {
+        Expression<Func<IQueryable<Person>, IQueryable<string>>> expression =
+            q => q.Select(x => x.Name);
 
+        Statement statement = ExpressionToStatementTranslatorStrategy.Translate(expression);
+
+        statement.Should().NotBeNull();
+
+        statement.Should().BeStatement<SelectStatement>();
+
+        SelectStatement s = (SelectStatement)statement;
+
+        s.TypeFrom.Should().Be<Person>();
+        s.TypeTo.Should().Be<string>();
+    }
 }
