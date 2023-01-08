9eecdbb36f48d80eddbc54224c4d76a094a3e831
stanac <stanacevm@gmail.com>
2023-01-08 12:43:16 +0100

    simple subquery working

--- tests/ArgoStore.IntegrationTests/SubqueryTests.cs
+++ tests/ArgoStore.IntegrationTests/SubqueryTests.cs
@@ -1,9 +1,33 @@
-﻿namespace ArgoStore.IntegrationTests;
+﻿using ArgoStore.TestsCommon.Entities;
+using ArgoStore.TestsCommon.TestData;
+
+namespace ArgoStore.IntegrationTests;
 
 public class SubqueryTests : IntegrationTestBase
 {
     public SubqueryTests()
     {
         InsertTestPersons();
+    }
+    
+    [Fact]
+    public void SimpleFromSubQuery_ToAnonymousObject_ToList_ReturnsExpected()
+    {
+        using IArgoQueryDocumentSession s = Store.OpenQuerySession();
+
+        var result = s.Query<Person>()
+            .Select(x => new { Birth = x.BirthYear, OldPoints = x.Points })
+            .Select(x => new { NewPoints = x.OldPoints, x.Birth })
+            .ToList();
+
+        List<Person> persons = PersonTestData.GetPersonTestData().ToList();
+
+        foreach (var r in result)
+        {
+            Person p = persons.SingleOrDefault(x => x.Points == r.NewPoints);
+            p.Should().NotBeNull();
+
+            r.Birth.Should().Be(p.BirthYear);
+        }
     }
 }
