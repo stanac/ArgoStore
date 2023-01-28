4dd061cc89229d6b3dcdf14b15a9612db3d6212a
stanac <stanacevm@gmail.com>
2023-01-28 21:52:28 +0100

    QueryOnNestedObjectTests

--- tests/ArgoStore.IntegrationTests/Tests/QueryOnNestedObjectTests.cs
+++ tests/ArgoStore.IntegrationTests/Tests/QueryOnNestedObjectTests.cs
@@ -1,4 +1,7 @@
-﻿namespace ArgoStore.IntegrationTests.Tests;
+﻿using ArgoStore.TestsCommon.Entities;
+using ArgoStore.TestsCommon.TestData;
+
+namespace ArgoStore.IntegrationTests.Tests;
 
 public class QueryOnNestedObjectTests : IntegrationTestBase
 {
@@ -8,8 +11,28 @@ public class QueryOnNestedObjectTests : IntegrationTestBase
     }
 
     [Fact]
-    public void Test1()
+    public void Test12()
     {
+        using IArgoQueryDocumentSession s = Store.OpenQuerySession();
 
+        List<Person> r = s.Query<Person>()
+            .Where(x => x.PrimaryContact.ContactType < 0)
+            .ToList();
+
+        var expected = PersonTestData.GetPersonTestData()
+            .Where(x => x.PrimaryContact != null && x.PrimaryContact.ContactType < 0)
+            .ToList();
+
+        r.Should().HaveCount(expected.Count);
     }
+
+    //[Fact]
+    //public void Test1()
+    //{
+    //    using IArgoQueryDocumentSession s = Store.OpenQuerySession();
+
+    //    List<Person> r = s.Query<Person>()
+    //        .Where(x => x.PrimaryContact.ContactInfos.Any(c => c.Details.Any(d => d == "s2")))
+    //        .ToList();
+    //}
 }
