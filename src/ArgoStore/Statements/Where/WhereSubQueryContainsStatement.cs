e7e4a0d6eb12f7f3a3b9f6168a7ab540117fda92
stanac <stanacevm@gmail.com>
2023-02-14 15:39:18 +0100

    ...

---
+++ src/ArgoStore/Statements/Where/WhereSubQueryContainsStatement.cs
@@ -0,0 +1,11 @@
+﻿namespace ArgoStore.Statements.Where;
+
+internal class WhereSubQueryContainsStatement : WhereSubQueryStatement
+{
+    public WhereSubQueryContainsStatement(FromAlias alias)
+    {
+        Alias = alias;
+    }
+
+    public override FromAlias Alias { get; }
+}
--- src/ArgoStore/Statements/Where/WhereSubQueryStatement.cs
+++ src/ArgoStore/Statements/Where/WhereSubQueryStatement.cs
@@ -4,28 +4,3 @@ internal abstract class WhereSubQueryStatement : WhereStatementBase
 {
     public abstract FromAlias Alias { get; }
 }
-
-internal class WhereSubQueryAnyStatement : WhereSubQueryStatement
-{
-    public WhereSubQueryFromStatement From { get; }
-    public WhereStatementBase? Condition { get; }
-    public override FromAlias Alias { get; }
-    
-    public WhereSubQueryAnyStatement(WhereSubQueryFromStatement from, WhereStatementBase? condition, FromAlias alias)
-    {
-        From = from;
-        Condition = condition;
-        Alias = alias;
-    }
-
-}
-
-internal class WhereSubQueryContainsStatement : WhereSubQueryStatement
-{
-    public WhereSubQueryContainsStatement(FromAlias alias)
-    {
-        Alias = alias;
-    }
-
-    public override FromAlias Alias { get; }
-}
